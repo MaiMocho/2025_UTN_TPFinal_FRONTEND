bea495d5676c2b78ea25b0572a43936b34c31c28
Maia.dev <asmodeoanxy@gmail.com>
2025-12-05 19:26:30 -0300

    chinga

--- src/Screens/WorkspaceScreen/WorkspaceScreen.jsx
+++ src/Screens/WorkspaceScreen/WorkspaceScreen.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
-import ChannelSidebar from '../../Components/ChannelSidebar/ChannelSidebar'
-import ChannelDetail from '../../Components/ChannelDetail/ChannelDetail'
+import { Link } from 'react-router'
+// Asegúrate de que la ruta de importación sea correcta
+import TaskList from '../../Components/TaskList/TaskList' 
 
 const WorkspaceScreen = () => {
   return (
-    <div>
-        <ChannelSidebar/>
-        <ChannelDetail/>
+    <div style={{ display: 'flex', height: '100vh' }}>
+        <aside style={{ width: '200px', background: '#f0f0f0', padding: '20px' }}>
+            <h3>Menú</h3>
+            <Link to="/home">⬅ Volver al Inicio</Link>
+        </aside>
+
+        <main style={{ flex: 1, padding: '20px', background: '#111' }}>
+            <TaskList />
+        </main>
     </div>
   )
 }
